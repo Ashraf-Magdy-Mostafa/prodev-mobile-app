5f6f507f228d00896ba03502e1a2c9666d5778b1
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-09-07 18:50:23 +0300

    checker fix 2

--- prodev-mobile-app-0x06/app/_layout.tsx
+++ prodev-mobile-app-0x06/app/_layout.tsx
@@ -4,9 +4,7 @@ import "../styles/global.css";
 
 export default function RootLayout() {
   return (
-    <Stack screenOptions={{
-      headerShown: false,
-    }}>
+    <Stack>
       <Stack.Screen name="index" />
       <Stack.Screen name="join" />
       <Stack.Screen name="signin" />
